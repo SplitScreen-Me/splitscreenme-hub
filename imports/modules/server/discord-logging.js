1993865f082e8d8cfc637af68419b5cac0b9000a
Rémi Mach <remi.mach@loginline.com>
2020-04-05 01:54:10 +0200

    Newly published handlers are also shown in #general

--- imports/modules/server/discord-logging.js
+++ imports/modules/server/discord-logging.js
@@ -3,6 +3,7 @@ import { HTTP } from 'meteor/http';
 
 const adminLogging_webhookUrl = Meteor.settings.private.DISCORD_ADMIN_LOGGING_WEBHOOK || "";
 const releaseLogging_webhookUrl = Meteor.settings.private.DISCORD_RELEASE_LOGGING_WEBHOOK || "";
+const generalLogging_webhookUrl = Meteor.settings.private.DISCORD_GENERAL_LOGGING_WEBHOOK || "";
 
 // Discord Admin Logging :
 const d_aLog = (title, description) => {
@@ -16,32 +17,37 @@ const d_aLog = (title, description) => {
 };
 // Discord Release Logging :
 const d_rLog = handler => {
-  if(releaseLogging_webhookUrl.length > 0){
+  const discordData = {
+    content: "**New handler release!**",
+    embeds: [{
+      color: 3447003,
+      title: handler.gameName,
+      url: `https://hub.splitscreen.me/handler/${handler._id}`,
+      thumbnail: {
+        url: `https://images.igdb.com/igdb/image/upload/t_cover_big/${handler.gameCover}.jpg`,
+      },
+      fields: [
+        {
+          name: 'Author',
+          inline: true,
+          value: handler.ownerName,
+        },
+      ],
+      timestamp: new Date(),
+      footer: {
+        icon_url: "https://www.splitscreen.me/img/splitscreen-me-logo.png",
+        text: '© SplitScreen.Me',
+      },
+    }],
+  };
+
+  if(releaseLogging_webhookUrl.length > 0 && generalLogging_webhookUrl){
     HTTP.call('POST', releaseLogging_webhookUrl, {
-      data: {
-        content: "**New handler release!**",
-        embeds: [{
-          color: 3447003,
-          title: handler.gameName,
-          url: `https://hub.splitscreen.me/handler/${handler._id}`,
-          thumbnail: {
-            url: `https://images.igdb.com/igdb/image/upload/t_cover_big/${handler.gameCover}.jpg`,
-          },
-          fields: [
-            {
-              name: 'Author',
-              inline: true,
-              value: handler.ownerName,
-            },
-          ],
-          timestamp: new Date(),
-          footer: {
-            icon_url: "https://www.splitscreen.me/img/splitscreen-me-logo.png",
-            text: '© SplitScreen.Me',
-          },
-        }],
-      }
-    })
+      data: discordData
+    });
+    HTTP.call('POST', generalLogging_webhookUrl, {
+      data: discordData
+    });
   }else{
     console.log(`[Discord Release Logging] ${handler.title} by ${handler.ownerName}`);
   }
